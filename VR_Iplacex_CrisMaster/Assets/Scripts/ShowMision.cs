d8b4c6d758eec29885a5b46133e5c58cfadb3a10
krist <82980687+Cris-Daiki@users.noreply.github.com>
2023-02-22 19:04:23 -0300

    Dialogo mision

---
+++ VR_Iplacex_CrisMaster/Assets/Scripts/ShowMision.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowMision : MonoBehaviour
+{
+    public List<string> TextValue;
+    //public Text textElement;
+    public GameObject Mision;
+    public TMP_Text textElement;
+    ShowTexto Numero;
+    public void Call(){
+        Numero = FindObjectOfType<ShowTexto>();
+        textElement.text = TextValue[Numero.NumeroAleatorio];
+    }
+    public void NoCAll(){
+        Mision.SetActive(false);
+    }
+}
